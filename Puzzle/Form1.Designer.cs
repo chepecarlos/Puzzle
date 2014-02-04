2a561b6d885d75148248cad003deae481246f62a
Evorock <evorock@alswblog.org>
2014-02-04 00:09:20 -0600

    Proyecto Entregado

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GB2 = new System.Windows.Forms.GroupBox();
+            this.GB = new System.Windows.Forms.GroupBox();
+            this.B22 = new System.Windows.Forms.Button();
+            this.B12 = new System.Windows.Forms.Button();
+            this.B02 = new System.Windows.Forms.Button();
+            this.B21 = new System.Windows.Forms.Button();
+            this.B11 = new System.Windows.Forms.Button();
+            this.B01 = new System.Windows.Forms.Button();
+            this.B20 = new System.Windows.Forms.Button();
+            this.B10 = new System.Windows.Forms.Button();
+            this.B00 = new System.Windows.Forms.Button();
+            this.GB1 = new System.Windows.Forms.GroupBox();
+            this.GB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GB2
+            // 
+            this.GB2.Location = new System.Drawing.Point(12, 102);
+            this.GB2.Name = "GB2";
+            this.GB2.Size = new System.Drawing.Size(177, 92);
+            this.GB2.TabIndex = 1;
+            this.GB2.TabStop = false;
+            this.GB2.Text = "LLLL";
+            // 
+            // GB
+            // 
+            this.GB.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.GB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GB.AutoSize = true;
+            this.GB.CausesValidation = false;
+            this.GB.Controls.Add(this.B22);
+            this.GB.Controls.Add(this.B12);
+            this.GB.Controls.Add(this.B02);
+            this.GB.Controls.Add(this.B21);
+            this.GB.Controls.Add(this.B11);
+            this.GB.Controls.Add(this.B01);
+            this.GB.Controls.Add(this.B20);
+            this.GB.Controls.Add(this.B10);
+            this.GB.Controls.Add(this.B00);
+            this.GB.Enabled = false;
+            this.GB.Location = new System.Drawing.Point(201, 12);
+            this.GB.Name = "GB";
+            this.GB.Size = new System.Drawing.Size(204, 191);
+            this.GB.TabIndex = 1;
+            this.GB.TabStop = false;
+            this.GB.Enter += new System.EventHandler(this.GB_Enter);
+            // 
+            // B22
+            // 
+            this.B22.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B22.Location = new System.Drawing.Point(138, 123);
+            this.B22.Name = "B22";
+            this.B22.Size = new System.Drawing.Size(60, 48);
+            this.B22.TabIndex = 8;
+            this.B22.UseVisualStyleBackColor = true;
+            // 
+            // B12
+            // 
+            this.B12.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B12.Location = new System.Drawing.Point(138, 70);
+            this.B12.Name = "B12";
+            this.B12.Size = new System.Drawing.Size(60, 48);
+            this.B12.TabIndex = 7;
+            this.B12.UseVisualStyleBackColor = true;
+            this.B12.Click += new System.EventHandler(this.B23_Click);
+            // 
+            // B02
+            // 
+            this.B02.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B02.Location = new System.Drawing.Point(138, 16);
+            this.B02.Name = "B02";
+            this.B02.Size = new System.Drawing.Size(60, 48);
+            this.B02.TabIndex = 6;
+            this.B02.UseVisualStyleBackColor = true;
+            // 
+            // B21
+            // 
+            this.B21.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B21.Location = new System.Drawing.Point(72, 124);
+            this.B21.Name = "B21";
+            this.B21.Size = new System.Drawing.Size(60, 48);
+            this.B21.TabIndex = 5;
+            this.B21.UseVisualStyleBackColor = true;
+            // 
+            // B11
+            // 
+            this.B11.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B11.Location = new System.Drawing.Point(72, 70);
+            this.B11.Name = "B11";
+            this.B11.Size = new System.Drawing.Size(60, 48);
+            this.B11.TabIndex = 4;
+            this.B11.UseVisualStyleBackColor = true;
+            // 
+            // B01
+            // 
+            this.B01.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B01.Location = new System.Drawing.Point(72, 16);
+            this.B01.Name = "B01";
+            this.B01.Size = new System.Drawing.Size(60, 48);
+            this.B01.TabIndex = 3;
+            this.B01.UseVisualStyleBackColor = true;
+            // 
+            // B20
+            // 
+            this.B20.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B20.Location = new System.Drawing.Point(6, 124);
+            this.B20.Name = "B20";
+            this.B20.Size = new System.Drawing.Size(60, 48);
+            this.B20.TabIndex = 2;
+            this.B20.UseVisualStyleBackColor = true;
+            this.B20.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // B10
+            // 
+            this.B10.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold);
+            this.B10.Location = new System.Drawing.Point(6, 70);
+            this.B10.Name = "B10";
+            this.B10.Size = new System.Drawing.Size(60, 48);
+            this.B10.TabIndex = 1;
+            this.B10.UseVisualStyleBackColor = true;
+            // 
+            // B00
+            // 
+            this.B00.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B00.Location = new System.Drawing.Point(6, 17);
+            this.B00.Name = "B00";
+            this.B00.Size = new System.Drawing.Size(60, 48);
+            this.B00.TabIndex = 0;
+            this.B00.UseVisualStyleBackColor = true;
+            this.B00.Click += new System.EventHandler(this.B00_Click);
+            this.B00.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TRUE);
+            // 
+            // GB1
+            // 
+            this.GB1.Location = new System.Drawing.Point(12, 12);
+            this.GB1.Name = "GB1";
+            this.GB1.Size = new System.Drawing.Size(177, 84);
+            this.GB1.TabIndex = 0;
+            this.GB1.TabStop = false;
+            this.GB1.Text = "MARCADOR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 206);
+            this.Controls.Add(this.GB2);
+            this.Controls.Add(this.GB);
+            this.Controls.Add(this.GB1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GB.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GB2;
+        private System.Windows.Forms.GroupBox GB;
+        private System.Windows.Forms.Button B22;
+        private System.Windows.Forms.Button B12;
+        private System.Windows.Forms.Button B02;
+        private System.Windows.Forms.Button B21;
+        private System.Windows.Forms.Button B11;
+        private System.Windows.Forms.Button B01;
+        private System.Windows.Forms.Button B20;
+        private System.Windows.Forms.Button B10;
+        private System.Windows.Forms.Button B00;
+        private System.Windows.Forms.GroupBox GB1;
+
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+        int Sx = 0;
+        int Sy = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TRUE(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            // Assign the event handler to the form.
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // Assign the event handler to the text box.
+            //this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyValue);
+
+            if (e.KeyCode.ToString() == "Left" )
+            {
+                OpLeft();
+                //this.KeyPreview = false;
+                //MessageBox.Show("KeyPreview is True, and this is from the FORM.");
+            
+            }
+            else if (e.KeyCode.ToString() == "Right")
+            {
+                OpRight();
+                //this.KeyPreview = false;
+                //MessageBox.Show("KeyPreview is True, and this is from the FORM.");
+
+            }
+            else if (e.KeyCode.ToString() == "Up")
+            {
+                //this.KeyPreview = false;
+                //MessageBox.Show("KeyPreview is True, and this is from the FORM.");
+                OpUp();
+
+            }
+            else if (e.KeyCode.ToString() == "Down")
+            {
+                OpDown();
+                //this.KeyPreview = false;
+                //MessageBox.Show("KeyPreview is True, and this is from the FORM.");
+
+            }
+            else if (e.KeyCode.ToString() == "R")
+            {
+
+                //MessageBox.Show("KeyPreview is True, and this is from the FORM.");
+
+                SuperRandon();
+              
+            }
+            else if (e.KeyCode.ToString() == "E")
+            {
+                if (Examen())
+                {
+                    MessageBox.Show("Felicidades :D");
+                }
+            }
+
+        }
+  
+        private void GB_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool Examen()
+        {
+            int[] lote = {1,8,7,
+                         2,0,6,
+                         3,4,5};
+            int i = 0;
+            bool Nota = true;
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (TuNombre(x, y) != lote[i])
+                    {
+                        MessageBox.Show("Aun le falta ordenar X:"+x.ToString()+" Y:"+y.ToString()+" valor "+lote[i].ToString());
+                        Nota = false;
+                        return Nota;
+                    }
+                    i++;
+                }
+            }
+            return Nota;
+        }
+
+        public void OpUp()
+        {
+            if (Sx != 0)
+            {
+                int tmp = TuNombre(Sx-1, Sy);
+                TeLlamaras(Sx, Sy, tmp);
+                TeLlamaras(Sx - 1, Sy, 0);
+            }
+
+        }
+
+        public void OpDown()
+        {
+            if (Sx != 2)
+            {
+                int tmp = TuNombre(Sx + 1, Sy);
+                TeLlamaras(Sx, Sy, tmp);
+                TeLlamaras(Sx + 1, Sy, 0);
+            }
+
+        }
+
+        public void OpRight()
+        {
+
+            if (Sy != 2)
+            {
+                int tmp = TuNombre(Sx, Sy + 1);
+                TeLlamaras(Sx, Sy, tmp);
+                TeLlamaras(Sx, Sy + 1, 0);
+
+            }
+            
+        }
+
+        public void OpLeft()
+        {
+            if (Sy != 0)
+            {
+                int tmp = TuNombre(Sx, Sy - 1);
+                TeLlamaras(Sx, Sy, tmp);
+                TeLlamaras(Sx, Sy - 1, 0);
+
+            }
+
+        }
+
+        public void SuperRandon() {
+            Random r = new Random();
+            int pollo = 0;
+            //int x = 0, y = 0;
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++) {
+                    TeLlamaras(x, y, 10);
+                }
+            }
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    do{
+                        pollo = r.Next(0,9);
+                        for (int xxx = 0; xxx < 3; xxx++)
+                        {
+                            for (int yyy = 0; yyy < 3; yyy++)
+                            {
+                                if (TuNombre(xxx, yyy) == pollo)
+                                {
+                                    pollo = 11;
+                                }
+                            }
+                        }
+                    }while(pollo > 8);
+                    TeLlamaras(x,y,pollo);
+                   
+                }
+            }
+            
+        }
+
+        public void Niegate(int x, int y, bool v)
+        {
+            if (x == 0 && y == 0)
+            {
+                B00.Visible = v;
+            }
+            else if (x == 0 && y == 1)
+            {
+                B01.Visible = v;
+            }
+            else if (x == 0 && y == 2)
+            {
+                B02.Visible = v;
+            }
+            else if (x == 1 && y == 0)
+            {
+                B10.Visible = v;
+            }
+            else if (x == 1 && y == 1)
+            {
+                B11.Visible = v;
+            }
+            else if (x == 1 && y == 2)
+            {
+                B12.Visible = v;
+            }
+            else if (x == 2 && y == 0)
+            {
+                B20.Visible = v;
+            }
+            else if (x == 2 && y == 1)
+            {
+                B21.Visible = v;
+            }
+            else if (x == 2 && y == 2)
+            {
+                B22.Visible = v;
+            }
+
+        }
+
+        public int TuNombre(int x, int y)
+        {
+            if (x == 0 && y == 0)
+            {
+                return Convert.ToInt16(B00.Text.ToString());
+            }
+            else if (x == 0 && y == 1)
+            {
+                return Convert.ToInt16(B01.Text.ToString());
+            }
+            else if (x == 0 && y == 2)
+            {
+                return Convert.ToInt16(B02.Text.ToString());
+            }
+            else if (x == 1 && y == 0)
+            {
+                return Convert.ToInt16(B10.Text.ToString());
+            }
+            else if (x == 1 && y == 1)
+            {
+                return Convert.ToInt16(B11.Text.ToString());
+            }
+            else if (x == 1 && y == 2)
+            {
+                return Convert.ToInt16(B12.Text.ToString());
+            }
+            else if (x == 2 && y == 0)
+            {
+                return Convert.ToInt16(B20.Text.ToString());
+            }
+            else if (x == 2 && y == 1)
+            {
+                return Convert.ToInt16(B21.Text.ToString());
+            }
+            //if (x == 2 && y == 2)
+                return Convert.ToInt16(B22.Text.ToString());
+            
+
+        }
+
+        public void TeLlamaras(int x, int y, int v) {
+
+            if (x == 0 && y == 0) {
+                B00.Text = v.ToString();
+            }
+            else if (x == 0 && y == 1)
+            {
+                B01.Text = v.ToString();
+            }
+            else if (x == 0 && y == 2)
+            {
+                B02.Text = v.ToString();
+            }
+            else if (x == 1 && y == 0)
+            {
+                B10.Text = v.ToString();
+            }
+            else if (x == 1 && y == 1)
+            {
+                B11.Text = v.ToString();
+            }
+            else if (x == 1 && y == 2)
+            {
+                B12.Text = v.ToString();
+            }
+            else if (x == 2 && y == 0)
+            {
+                B20.Text = v.ToString();
+            }
+            else if (x == 2 && y == 1)
+            {
+                B21.Text = v.ToString();
+            }
+            else if (x == 2 && y == 2)
+            {
+                B22.Text = v.ToString();
+            }
+
+            if (v == 0)
+            {
+                Niegate(x, y, false);
+                Sx = x;
+                Sy = y;
+            }
+            else
+                Niegate(x, y, true);
+        }
+
+        private void B23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B00_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
